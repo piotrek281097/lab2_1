83c0d124eb6cc5067a9423a48341ff90c350e2be
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-03-24 22:16:14 +0100

    testy_v1 test binarySearchShouldReturnThatElementIsFoundInSequenceOf1

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,34 @@
+package edu.iis.mto.bsearch;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.empty;
+import static org.junit.Assert.*;
+
+public class BinarySearchTest {
+
+    private static final int wantedElement = 8;
+    private static final int[] seqTest1 = {8};
+
+    private SearchResult searchResult;
+
+    @Before
+    public void setup() {
+        searchResult = new SearchResult();
+    }
+
+    @Test
+    public void binarySearchShouldReturnThatElementIsFoundInSequenceOf1() {
+        searchResult = BinarySearch.search(wantedElement, seqTest1);
+        assertTrue(searchResult.isFound());
+    }
+
+
+
+
+
+}
